af5f5633e48790d20cbce9e5776ae95474309475
Nikhil-BV <nikhilbv.xworkz@gmail.com>
2022-06-21 09:38:50 +0530

    committing Amazomprime file

---
+++ Amazomprime.java
@@ -0,0 +1,56 @@
+class Amazonprime
+{
+  static String name ="KGF 2";
+  static String name1="Heropanti 2";
+  static String name2="Acharya";
+  static String name3="Pushpa";
+  static String name4="Visithiran";
+  static String name5="Moonfal";
+  static String name6="KGF1";
+  static String name7="Wronr turn";
+  static String name8="radhe Shyam";
+  static String name9="FIR";
+  static String name10="Deep Water";
+  static String name11="Uncharted";
+  static String name12="Mahaan";
+  static String name13="Yuvarathnaa";
+  static String name14="Dune";
+  static String name15="After";
+  static String name16="Master";
+  static String name17="Saaho";
+  static String name18="V";
+  static String name19="Trance";
+  static String name20="Home";
+  static String name21="Hit";
+  static String name22="Makki";
+  static String name23="Naani";
+  static String name24="Tom and Jerrry";
+public static void main (String a[])
+{
+ System.out.println("The movie name is "+ name);
+ System.out.println("The movie name1 is "+ name1);
+ System.out.println("The movie name2 is "+ name2);
+ System.out.println("The movie name3 is "+ name3);
+ System.out.println("The movie name4 is "+ name4);
+ System.out.println("The movie name5 is "+ name5);
+ System.out.println("The movie name6 is "+ name6);
+ System.out.println("The movie name7 is "+ name7);
+ System.out.println("The movie name8 is "+ name8);
+ System.out.println("The movie name9 is "+ name9);
+ System.out.println("The movie name10 is "+ name10);
+ System.out.println("The movie name11 is "+ name11);
+ System.out.println("The movie name12 is "+ name12);
+ System.out.println("The movie name13 is "+ name13);
+ System.out.println("The movie name14 is "+ name14);
+ System.out.println("The movie name15 is "+ name15);
+ System.out.println("The movie name16 is "+ name16);
+ System.out.println("The movie name17 is "+ name17);
+ System.out.println("The movie name18 is "+ name18);
+ System.out.println("The movie name19 is "+ name19);
+ System.out.println("The movie name20 is "+ name20);
+ System.out.println("The movie name21 is "+ name21);
+ System.out.println("The movie name22 is "+ name22);
+ System.out.println("The movie name23 is "+ name23);
+ System.out.println("The movie name24 is "+ name24);
+}
+}
